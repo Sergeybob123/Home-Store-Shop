aa8859363b0ea62ce2f6ad5eb84bdf601671376b
Sergey Babchenkov <144060876+Sergeybob123@users.noreply.github.com>
2024-04-22 18:00:44 +0300

    example

---
+++ main/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    context ={
+        'title': 'Home',
+        'content': 'Главная страница',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'bool': True,
+    }
+    
+    return render(request, 'main/index.html', context)
+
+
+def about(request):
+    return HttpResponse('About page')
