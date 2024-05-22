37f146539e8f73d74fd96a2f866efc482747621c
Sergey Babchenkov <144060876+Sergeybob123@users.noreply.github.com>
2024-05-22 18:52:53 +0300

    login required decorator - redirect to target page

--- app/settings.py
+++ app/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from django.conf.global_settings import LOGIN_URL
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,3 +152,4 @@ INTERNAL_IPS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+LOGIN_URL = '/user/login/'
