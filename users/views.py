2256f5b414386352e4f0bc6f3c1c17744262f7d2
Sergey Babchenkov <144060876+Sergeybob123@users.noreply.github.com>
2024-06-19 18:55:45 +0300

    Add to cart view, refactor included cart templates

--- users/views.py
+++ users/views.py
@@ -17,7 +17,8 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
 
-                if request.POST.get('next', None):
+                if request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 
                 return HttpResponseRedirect(reverse('main:index'))
