94c9173efcaa165b8d5e0070c8f1da3d8f1004a8
Sergey Babchenkov <144060876+Sergeybob123@users.noreply.github.com>
2024-07-09 17:44:59 +0300

    Cart remove, controller added, commit before ajax

--- users/views.py
+++ users/views.py
@@ -17,7 +17,7 @@ def login(request):
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт")
 
-                if request.POST.get('next', None)
+                redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
                     return HttpResponseRedirect(request.POST.get('next'))
                 
